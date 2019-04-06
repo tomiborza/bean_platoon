7e0e454eed368b9b1bc98d39828e11af19cb4621
tomiborza <borzatamas00@gmail.com>
2019-04-06 17:52:26 +0300

    temp sensor working on switch

--- main/main.ino
+++ main/main.ino
@@ -16,21 +16,25 @@ void setup(){
 void loop(){
   //start of temperaturehumidity sensor
   int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
   //end of temperature humidity sensor
-  Serial.println(analogRead(A0));  //soil humidity
-  s_val=digitalRead(s_onoff);      
-  if(s_val==LOW ){      //switching the system on/off
-  analogWrite(piros,LOW);
-  analogWrite(zold,20);
-  
-  }else {
-  analogWrite(zold,LOW);                  
-  analogWrite(piros,10);
-  Serial.println(s_val);
-  
+  //Serial.println(analogRead(A0));  //soil humidity
+  s_val=digitalRead(s_onoff);
+  s_val=!s_val;      
+  if(s_val==HIGH ){      //switching the system on/off
+    
+    analogWrite(piros,LOW);
+    analogWrite(zold,20);
+    Serial.println(s_val);
+    
+    Serial.print("Temperature = ");
+    Serial.println(DHT.temperature);
+    Serial.print("Humidity = ");
+    Serial.println(DHT.humidity);
+  }else{
+    analogWrite(zold,LOW);                  
+    analogWrite(piros,10);
+    Serial.println(s_val);
+    
   }
+  delay(500);
 }
