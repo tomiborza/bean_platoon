7325a85cce9acaafcbcd0f638c47bc80bb5399f3
tomiborza <borzatamas00@gmail.com>
2019-04-06 17:29:53 +0300

    Temperature sensor added

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,6 @@
+#include <dht.h>
+#define DHT11_PIN 7
+dht DHT;
 int s_onoff=8; // switch pin
 bool s_val; //switch value
 int zold=5;
@@ -11,7 +14,13 @@ void setup(){
 }
 
 void loop(){
-  
+  //start of temperaturehumidity sensor
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  //end of temperature humidity sensor
   Serial.println(analogRead(A0));  //soil humidity
   s_val=digitalRead(s_onoff);      
   if(s_val==LOW ){      //switching the system on/off
